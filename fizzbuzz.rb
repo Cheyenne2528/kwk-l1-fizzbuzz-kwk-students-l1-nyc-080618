b828550b1b8aca85637f52c967fb1fcb213c3fa0
Cheyenne2528 <cheyennefun12@gmail.com>
2018-08-07 20:11:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,15 @@
 def fizzbuzz(number)
 
-if number%3 == 0
+if number%15 == 0
+  return "FizzBuzz"
+  
+elsifif number%3 == 0
   return "Fizz"
 
 elsif number%5 == 0
   return "Buzz"
 
-elsif number%15 == 0
-  return "FizzBuzz"
+
 end
 end
 fizzbuzz(15)
